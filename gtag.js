7525498908ba185d1f7d81d919dedb4a711a8e0f
Arikaim <sales@intersoft.biz>
2020-12-02 09:06:30 +0200

    =

--- gtag.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-var libraryScript = document.getElementById('library_analytics');
-var trackingId = libraryScript.getAttribute('tracking-id');
-
-gtag('config',trackingId);
